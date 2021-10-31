98985eda4ee9e56a907a5dfec58a34e4dd638634
pablop76 <pawelpoltoraczyk@gmail.com>
2021-10-31 11:27:54 +0100

    css

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -3,20 +3,20 @@ const Quiz = (props) => {
     const { currentTest, currentQuestion, isDisabled, answerChange } = props;
     return (
         <>
-            <div className="p-8 text-2xl text-center bg-yellow-500 text-gray-800 rounded-full max-w-xl mx-auto m-5 font-bold">
+            <div className="p-5 text-2xl text-center bg-yellow-500 text-gray-800 rounded-3xl max-w-xl mx-auto m-5 font-bold">
                 {currentTest[currentQuestion]?.question}
-                {currentTest[currentQuestion]?.image ? <img src={`./${currentTest[currentQuestion]?.image}`} alt="" className="mx-auto" /> : ""}
+                <div className="my-5">{currentTest[currentQuestion]?.image ? <img src={`./${currentTest[currentQuestion]?.image}`} alt="" className="mx-auto" /> : ""}</div>
             </div>
             {props.children}
             <div className="container max-w-md p-3 mx-auto">
                 {currentTest[currentQuestion]?.content.map((answer, index) =>
                     <button
                         key={index}
-                        className="flex cursor-pointer m-5 answer items-center justify-center rounded-full bg-blue-500"
+                        className="flex cursor-pointer m-5 answer items-center justify-left rounded-3xl bg-blue-800 mx-auto w-full"
                         onClick={(el) => answerChange(index, el)}
                         disabled={isDisabled}
                     >
-                        <div className="rounded-full h-10 w-10 flex items-center justify-center bg-gray-700 text-white font-bold p-4">
+                        <div className="rounded-full h-10 w-10 flex items-center justify-center bg-blue-500 text-white font-bold p-4">
                             {index + 1}
                         </div>
                         <div className="py-2 px-4 text-white font-semibold text-left">
