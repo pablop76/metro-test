3bfbb99cba48aa2adf59c0fb5aad8f05cc3f4818
pablop76 <pawelpoltoraczyk@gmail.com>
2023-03-04 00:46:50 +0100

    poprawki w odpowiedziach dodanie kategorii skoda

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ import Quiz from "./components/quiz/Quiz.js";
 import ChoiceTest from "./components/choiceTest/ChoiceTest";
 import Footer from "./components/footer/Footer";
 import WrongAnswers from "./components/wrongAnswers/WrongAnswers";
-import { useEffect, useState } from 'react';
-import './App.css';
-import oklaski from './sound/oklaski.mp3';
-import smiech from './sound/smiech.mp3';
+import { useEffect, useState } from "react";
+import "./App.css";
+import oklaski from "./sound/oklaski.mp3";
+import smiech from "./sound/smiech.mp3";
 import StartModal from "./components/alerts/StartModal";
 
 function App() {
-
   const [currentTest, setCurrentTest] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [maxQuestions, setMaxQuestions] = useState(0);
@@ -32,7 +31,8 @@ function App() {
   const [allLimit, setAllLimit] = useState(0);
   const [inspiroLimit, setInspiroLimit] = useState(0);
   const [sygnalizacjaLimit, setSygnalizacjaLimit] = useState(0);
-  const [najnowszeLimit, setNajnowszeLimit] = useState(0);
+  const [liniaNr2Limit, setLiniaNr2Limit] = useState(0);
+  const [skodaLimit, setSkodaLimit] = useState(0);
   // zapisuje błedne odpowiedzi ale jeszcze nic z nimi nie robi
   const [wrongAnswers, setWrongAnswers] = useState([]);
   const [showWrongAnswers, setShowWrongAnswers] = useState(false);
@@ -40,11 +40,11 @@ function App() {
   // ustawienie tematu testu
   const handleTest = (e) => {
     setTest(e.target.value);
-  }
+  };
 
   const handleModal = () => {
     setModal("none");
-  }
+  };
 
   // Losowanie pytań od 0 do długość tablicy
   const draw = (arr, counter) => {
@@ -61,12 +61,11 @@ function App() {
       return;
     }
     return arr3;
-  }
+  };
   const sound = new Audio(oklaski);
   const sound2 = new Audio(smiech);
 
   const answerChange = (answerUser, el) => {
-
     if (currentQuestion >= maxQuestions + 1) return;
     setIsDisabled(true);
     let answerDiv = el.currentTarget;
@@ -76,7 +75,6 @@ function App() {
       setCorectAnswers(correctAnswers + 1);
       answerDiv.classList.add("current");
       setSuccesAlert(true);
-
     } else {
       if (audio) sound2.play();
       answerDiv.classList.add("wrong");
@@ -84,12 +82,14 @@ function App() {
       if (inCorrectAnswers + correctAnswers < maxQuestions) {
         setInCorrectAnswers(inCorrectAnswers + 1);
         // zbieranie niepoprawnych odpowiedzi
-        setSaveInCorrectAnswers([...saveInCorrectAnswers, currentTest[currentQuestion]]);
+        setSaveInCorrectAnswers([
+          ...saveInCorrectAnswers,
+          currentTest[currentQuestion],
+        ]);
       }
-      setWrongAnswers([...wrongAnswers, currentTest[currentQuestion]])
+      setWrongAnswers([...wrongAnswers, currentTest[currentQuestion]]);
     }
-
-  }
+  };
 
   const nextQuestion = () => {
     if (currentQuestion + 1 === maxQuestions) {
@@ -97,23 +97,23 @@ function App() {
       setSuccesAlert(false);
       setEndTest(true);
       return;
-    };
+    }
     setCurrentQuestion(currentQuestion + 1);
     document.querySelector(".wrong")?.classList.remove("wrong");
     document.querySelector(".current")?.classList.remove("current");
     setDangerAlert(false);
     setSuccesAlert(false);
     setIsDisabled(false);
-  }
+  };
 
-  const handleChangeLimit = event => {
+  const handleChangeLimit = (event) => {
     let { value, min, max } = event.target;
     value = Math.max(Number(min), Math.min(Number(max), Number(value)));
     if (value === 0) {
       setMaxQuestions("");
       return;
     }
-    setMaxQuestions(value)
+    setMaxQuestions(value);
   };
 
   const handleClickAudio = () => {
@@ -121,15 +121,14 @@ function App() {
   };
   const refreshPage = () => {
     window.location.reload(false);
-  }
+  };
 
   let colorSend = (() => {
-    if (Math.round(correctAnswers / maxQuestions * 100) >= 75) {
+    if (Math.round((correctAnswers / maxQuestions) * 100) >= 75) {
       return "bg-green-700";
     }
     return "bg-red-600";
   })();
-
 
   useEffect(() => {
     const startApp = (data, drawData, expr) => {
@@ -138,32 +137,34 @@ function App() {
       setMaxQuestions(data[expr].length);
       setCorectAnswers(0);
       setInCorrectAnswers(0);
-    }
+    };
     const getQuizData = async (expr) => {
-
-      const response = await fetch('./questions.json');
+      const response = await fetch("./questions.json");
       const data = await response.json();
       setAllLimit(data.all.length);
       setInspiroLimit(data.inspiro.length);
       setSygnalizacjaLimit(data.sygnalizacja.length);
-      setNajnowszeLimit(data.linia2.length);
+      setLiniaNr2Limit(data.linia2.length);
+      setSkodaLimit(data.skoda.length);
 
       let drawData = [];
       switch (expr) {
-        case 'inspiro':
+        case "inspiro":
           startApp(data, drawData, expr);
           break;
-        case 'sygnalizacja':
+        case "sygnalizacja":
           startApp(data, drawData, expr);
           break;
-        case 'linia2':
+        case "linia2":
+          startApp(data, drawData, expr);
+          break;
+        case "skoda":
           startApp(data, drawData, expr);
           break;
         default:
           startApp(data, drawData, expr);
       }
-
-    }
+    };
     getQuizData(test);
   }, [test]);
 
@@ -177,36 +178,114 @@ function App() {
       </div>
       <div className="flex justify-center text-white flex-wrap bg-overlay-top flex-grow">
         <div className="text-center">
-          <LimitOfquestions handleChangeLimit={handleChangeLimit} maxQuestions={maxQuestions} currentTest={currentTest} >
-            <ChoiceTest handleTest={handleTest} test={test} questionslimit={[allLimit, inspiroLimit, sygnalizacjaLimit, najnowszeLimit]} />
+          <LimitOfquestions
+            handleChangeLimit={handleChangeLimit}
+            maxQuestions={maxQuestions}
+            currentTest={currentTest}
+          >
+            <ChoiceTest
+              handleTest={handleTest}
+              test={test}
+              questionslimit={[
+                allLimit,
+                inspiroLimit,
+                sygnalizacjaLimit,
+                liniaNr2Limit,
+                skodaLimit,
+              ]}
+            />
           </LimitOfquestions>
         </div>
       </div>
-      {showWrongAnswers ? <WrongAnswers wrongAnswers={wrongAnswers} /> :
-        <Quiz currentTest={currentTest} currentQuestion={currentQuestion} answerChange={answerChange} isDisabled={isDisabled}>
-          {dangerAlert ? <DangerAlert answers={currentTest[currentQuestion].content} corectAnswer={currentTest[currentQuestion].correct} nextQuestion={nextQuestion} /> : ""}
+      {showWrongAnswers ? (
+        <WrongAnswers wrongAnswers={wrongAnswers} />
+      ) : (
+        <Quiz
+          currentTest={currentTest}
+          currentQuestion={currentQuestion}
+          answerChange={answerChange}
+          isDisabled={isDisabled}
+        >
+          {dangerAlert ? (
+            <DangerAlert
+              answers={currentTest[currentQuestion].content}
+              corectAnswer={currentTest[currentQuestion].correct}
+              nextQuestion={nextQuestion}
+            />
+          ) : (
+            ""
+          )}
           {succesAlert ? <SuccesAlert nextQuestion={nextQuestion} /> : ""}
-          {endTest ? <EndTestAlert correctAnswers={correctAnswers} inCorrectAnswers={inCorrectAnswers} maxQuestions={maxQuestions} colorSend={colorSend}>
-            <div className={"mx-auto"}>
-              <Refresh refreshPage={refreshPage} />
-            </div>
-            <button className={"bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"} onClick={() => {
-              setShowWrongAnswers(true);
-              setEndTest(false)
-            }}>Pokaż błędne odpowiedzi</button>
-          </EndTestAlert> : ""};
-        </Quiz>}
+          {endTest ? (
+            <EndTestAlert
+              correctAnswers={correctAnswers}
+              inCorrectAnswers={inCorrectAnswers}
+              maxQuestions={maxQuestions}
+              colorSend={colorSend}
+            >
+              <div className={"mx-auto"}>
+                <Refresh refreshPage={refreshPage} />
+              </div>
+              <button
+                className={
+                  "bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                }
+                onClick={() => {
+                  setShowWrongAnswers(true);
+                  setEndTest(false);
+                }}
+              >
+                Pokaż błędne odpowiedzi
+              </button>
+            </EndTestAlert>
+          ) : (
+            ""
+          )}
+          ;
+        </Quiz>
+      )}
       <div className="flex justify-center p-5 text-2xl bg-blue-800 text-white rounded-full max-w-xs mx-auto m-5">
         odpowiedzi {correctAnswers + inCorrectAnswers} / {maxQuestions}
       </div>
       <div className="flex items-center justify-center text-3xl bg-white rounded-full w-auto p-2 m-5 mx-auto">
-        <span style={{ color: 'green', display: 'flex', alignItems: 'center' }}><svg className="h-8 w-8 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-        </svg>{correctAnswers}:</span><span style={{ color: 'red', display: 'flex', alignItems: 'center' }}>{inCorrectAnswers}<svg className="h-8 w-8 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
-        </svg></span>
+        <span style={{ color: "green", display: "flex", alignItems: "center" }}>
+          <svg
+            className="h-8 w-8 text-green-600"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+            />
+          </svg>
+          {correctAnswers}:
+        </span>
+        <span style={{ color: "red", display: "flex", alignItems: "center" }}>
+          {inCorrectAnswers}
+          <svg
+            className="h-8 w-8 text-red-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
+            />
+          </svg>
+        </span>
       </div>
-      <div className={`flex items-baseline justify-center text-4xl ${colorSend} text-white rounded-full w-32 m-5 mx-auto p-2`}>{maxQuestions ? Math.round(correctAnswers / maxQuestions * 100) : ""}%</div>
+      <div
+        className={`flex items-baseline justify-center text-4xl ${colorSend} text-white rounded-full w-32 m-5 mx-auto p-2`}
+      >
+        {maxQuestions ? Math.round((correctAnswers / maxQuestions) * 100) : ""}%
+      </div>
       <Footer />
     </div>
   );
--- src/components/choiceTest/ChoiceTest.js
+++ src/components/choiceTest/ChoiceTest.js
@@ -1,28 +1,67 @@
 const ChoiceTest = (props) => {
-    const { handleTest, test, questionslimit } = props;
-    return (
-        <form className="mt-4">
-            <span className="text-yellow-400 text-xl">Wybór zakresu pytań:</span>
-            <div className="mt-2">
-                <label className="inline-flex items-center">
-                    <input type="radio" className="form-radio" name="choiceTest" value="all" checked={test === "all"} onChange={handleTest} />
-                    <span className="ml-2">Wszystko ({questionslimit[0]})</span>
-                </label>
-                <label className="inline-flex items-center ml-3">
-                    <input type="radio" className="form-radio" name="choiceTest" value="inspiro" checked={test === "inspiro"} onChange={handleTest} />
-                    <span className="ml-2">Inspiro ({questionslimit[1]})</span>
-                </label>
-                <label className="inline-flex items-center ml-3">
-                    <input type="radio" className="form-radio" name="choiceTest" value="sygnalizacja" checked={test === "sygnalizacja"} onChange={handleTest} />
-                    <span className="ml-2">Sygnalizacja ({questionslimit[2]})</span>
-                </label>
-                <label className="inline-flex items-center ml-3">
-                    <input type="radio" className="form-radio" name="choiceTest" value="linia2" checked={test === "linia2"} onChange={handleTest} />
-                    <span className="ml-2">Linia nr 2 ({questionslimit[3]})</span>
-                </label>
-            </div>
-        </form>
-    );
-}
+  const { handleTest, test, questionslimit } = props;
+  return (
+    <form className="mt-4">
+      <span className="text-yellow-400 text-xl">Wybór zakresu pytań:</span>
+      <div className="mt-2">
+        <label className="inline-flex items-center">
+          <input
+            type="radio"
+            className="form-radio"
+            name="choiceTest"
+            value="all"
+            checked={test === "all"}
+            onChange={handleTest}
+          />
+          <span className="ml-2">Wszystko ({questionslimit[0]})</span>
+        </label>
+        <label className="inline-flex items-center ml-3">
+          <input
+            type="radio"
+            className="form-radio"
+            name="choiceTest"
+            value="inspiro"
+            checked={test === "inspiro"}
+            onChange={handleTest}
+          />
+          <span className="ml-2">Inspiro ({questionslimit[1]})</span>
+        </label>
+        <label className="inline-flex items-center ml-3">
+          <input
+            type="radio"
+            className="form-radio"
+            name="choiceTest"
+            value="sygnalizacja"
+            checked={test === "sygnalizacja"}
+            onChange={handleTest}
+          />
+          <span className="ml-2">Sygnalizacja ({questionslimit[2]})</span>
+        </label>
+        <label className="inline-flex items-center ml-3">
+          <input
+            type="radio"
+            className="form-radio"
+            name="choiceTest"
+            value="linia2"
+            checked={test === "linia2"}
+            onChange={handleTest}
+          />
+          <span className="ml-2">Linia nr 2 ({questionslimit[3]})</span>
+        </label>
+        <label className="inline-flex items-center ml-3">
+          <input
+            type="radio"
+            className="form-radio"
+            name="choiceTest"
+            value="skoda"
+            checked={test === "skoda"}
+            onChange={handleTest}
+          />
+          <span className="ml-2">Skoda ({questionslimit[4]})</span>
+        </label>
+      </div>
+    </form>
+  );
+};
 
 export default ChoiceTest;
